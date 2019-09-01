194d7bef4fd4c3cecc17b85b9b82ca238fa13969
Richel Sensineli <richelsensineli@gmail.com>
2019-08-31 23:30:15 -0300

    Adicionado SplashScreen, NewContactScreen

---
+++ app/src/main/java/br/com/fiap/mycontactlist/list/ContactListActivity.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.mycontactlist.list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.mycontactlist.R
+
+class ContactListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_list)
+    }
+}
