d09e8d1002c9de1bdd836f7bcaa64f8bda2c9713
Richel Sensineli <richelsensineli@gmail.com>
2019-08-31 23:44:31 -0300

    Adicionado Fullscreen style

--- app/src/main/java/br/com/fiap/mycontactlist/MainActivity.kt
+++ app/src/main/java/br/com/fiap/mycontactlist/MainActivity.kt
@@ -1,12 +1,26 @@
 package br.com.fiap.mycontactlist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.fiap.mycontactlist.form.NewContactActivity
+import br.com.fiap.mycontactlist.list.ContactListActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btListAll.setOnClickListener {
+            val proximaTela =  Intent(this@MainActivity, ContactListActivity::class.java)
+            startActivity(proximaTela)
+        }
+
+        btNewContact.setOnClickListener {
+            val proximaTela =  Intent(this@MainActivity, NewContactActivity::class.java)
+            startActivity(proximaTela)
+        }
     }
 }
